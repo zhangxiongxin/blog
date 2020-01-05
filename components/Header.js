daa351cd64df322ef3129d893b17fe1f281d2825
zhangxiongxin <986192524@qq.com>
2020-01-05 23:12:43 +0800

    add

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../static/style/components/header.css'
+// import styles from '../static/style/components/header.css'
+import {Row,Col, Menu, Icon} from 'antd'
+const Header = () => (
+  <div className="header">
+    <Row type="flex" justify="center">
+        <Col  xs={24} sm={24} md={10} lg={10} xl={10}>
+            <span className="header-logo">enchanter</span>
+            <span className="header-txt">既要抬头看天，也要低头走路</span>
+        </Col>
+
+        <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
+            <Menu  mode="horizontal">
+                <Menu.Item key="home">
+                    <Icon type="home" />
+                    首页
+                </Menu.Item>
+                <Menu.Item key="video">
+                    <Icon type="youtube" />
+                    视频
+                </Menu.Item>
+                <Menu.Item key="life">
+                    <Icon type="smile" />
+                    生活
+                </Menu.Item>
+            </Menu>
+        </Col>
+    </Row>
+ </div>
+)
+
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import Head from 'next/head'
+import {Row, Col , List ,Icon} from 'antd'
+import axios from 'axios'
+import Header from '../components/Header'
+import '../static/style/pages/index.css'
+
+const Home = (list) => {
+  console.dir(list)
+  const [mylist] = useState(list.data.result)
+  console.log(mylist)
+  return(
+    <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <Header />
+    <Row className="comm-main" type="flex" justify="center">
+      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
+      <div>    
+      <List
+        header={<div>最新日志</div>}
+        itemLayout="vertical"
+        dataSource={mylist}
+        renderItem={item => (
+          <List.Item>
+            <div className="list-title">{item.title}</div>
+            <div className="list-icon">
+              <span><Icon type="calendar" /> {item.createTime}</span>
+              <span><Icon type="folder" /> 视频教程</span>
+              <span><Icon type="fire" /> 5498人</span>
+            </div>
+            <div className="list-context">{item.desc}</div>  
+          </List.Item>
+        )}
+      />    
+    </div>
+      </Col>
+
+      <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+        右侧
+      </Col>
+    </Row>
+
+ </div>
+  )
+
+}
+Home.getInitialProps = async ()=>{
+  const promise = new Promise((resolve)=>{
+    axios('http://rap2api.taobao.org/app/mock/241764/article').then(
+      (res)=>{
+        resolve(res.data)
+      }
+    )
+  })
+  return await promise
+}
+// Home.getInitialProps = async ()=>{
+//   const promise = new Promise((resolve)=>{
+//     axios('http://127.0.0.1:7001/default/getArticleList').then(
+//       (res)=>{
+//         resolve(res.data)
+//       }
+//     )
+//   })
+//   return await promise
+// }
+export default Home
